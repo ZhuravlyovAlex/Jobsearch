9571d7d2164cbb2b6d0c867009a61d629cd9a146
Aleksey <73385486+ZhuravlyovAlex@users.noreply.github.com>
2021-07-25 21:59:49 +0300

    Delete jobsearch-develop directory

--- jobsearch-develop/jobsearch-develop/src/main/java/ru/innopolis/jobsearch/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.innopolis.jobsearch.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.innopolis.jobsearch.entity.Company;
-
-public interface CompanyRepository extends JpaRepository<Company, Integer> {
-}
--- jobsearch-develop/jobsearch-develop/src/main/java/ru/innopolis/jobsearch/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.innopolis.jobsearch.repository;
-
-
-import ru.innopolis.jobsearch.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
